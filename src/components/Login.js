53517073835bc91abe6a5699a21e054863dd504f
Marten Sievers <ms@tjek.com>
2023-05-10 23:47:37 +0200

    feat: authentication

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = (props) => (
+    <nav className="login">
+        <h2>Inventory Login</h2>
+        <p>Sign in to manage your store's inventory.</p>
+        <button className="github" onClick={() => props.authenticate('Github')}>Log In With Github</button>
+        <button className="facebook" onClick={() => props.authenticate('Facebook')}>Log In With Facebook</button>
+        <button className="twitter" onClick={() => props.authenticate('Twitter')}>Log In With Twitter</button>
+    </nav>
+)
+export default Login;
