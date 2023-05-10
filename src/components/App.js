5acb2c661aa1784fea4c951b9daf2f4b2898111a
Marten Sievers <ms@tjek.com>
2023-05-10 21:18:14 +0200

    refactor: simplify destructuring of storeId

--- src/components/App.js
+++ src/components/App.js
@@ -12,8 +12,8 @@ class App extends React.Component {
         order: {}
     };
     componentDidMount() {
-        const { params } = this.props.match;
-        this.ref = base.syncState(`${params.storeId}/fishes`, {
+        const { storeId } = this.props.match.params;
+        this.ref = base.syncState(`${storeId}/fishes`, {
             context: this,
             state: 'fishes',
         });
